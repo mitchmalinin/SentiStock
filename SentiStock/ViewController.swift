de8b1ff292716f48c764723f72d5f2f5c9dae8b0
Justin Matsnev <jmatsnev@gmail.com>
2015-09-12 15:47:31 -0400

    rss feed is in

--- SentiStock/ViewController.swift
+++ SentiStock/ViewController.swift
@@ -21,7 +21,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet var tableView : UITableView!
     var xmlParser : XMLParser!
+    var defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
+    var data = NSMutableData()
+    var url : NSURL!
+    let stringUrl = "http://appleinsider.com/rss/news/"
+    var titleArticle: String!
+    var titleDescrip : String!
+    
+    var datastring : String?
+    
     let arrayy :NSMutableArray = []
+    let arrayDescrip :NSMutableArray = []
+    let arraySentiment : NSMutableArray = []
     
     override func viewDidLoad() {
      
@@ -29,6 +40,34 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         xmlParser = XMLParser()
         xmlParser.delegate = self
+        url = NSURL(string:stringUrl )
+
+        xmlParser.startParsingWithContentsOfURL(url!)
+
+        
+        
+        //Assining RSS Value to a dictionary
+        let currentDictionary = xmlParser.arrParsedData
+        println(xmlParser.arrParsedData)
+        
+        //Goes over all the dictionaries in the array
+        for var i=0; i<currentDictionary.count; i++ {
+            
+            //Checks fields of the dictionary
+            for (field, value) in currentDictionary[i] {
+                if (field == "title" && value.rangeOfString("Apple") != nil) {
+                    
+                    //Arrays for titles and descirptions get set up
+                    arrayy.addObject(currentDictionary[i]["title"]!)
+                    arrayDescrip.addObject(currentDictionary[i]["title"]!)
+                    
+                    
+                    
+                    
+                }
+            }
+            
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -45,14 +84,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return 1
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return arrayy.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier:"cell")
         
-//        cell.textLabel?.text = self.arrayy[indexPath.row] as? String
+       cell.textLabel?.text = self.arrayy[indexPath.row] as? String
         
         return cell
         
