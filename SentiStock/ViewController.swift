40a0ff2bb74201d4867af70087148b6a0f936436
Mitchell Malinin <mm10dog@gmail.com>
2015-09-12 13:45:31 -0400

    Initial Commit

---
+++ SentiStock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SentiStock
+//
+//  Created by Mitchell Malinin on 9/12/15.
+//  Copyright (c) 2015 Mitchell Malinin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
